19a981980230bce89ee01dfe52538c2d4a6f1536
Harsh M <hm855@snu.edu.in>
2021-11-30 02:14:29 -0700

    Uploading project skeleton, to add elements in next commit

---
+++ app/src/main/java/com/example/masterapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.masterapplication;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
+    }
+}
